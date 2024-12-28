bfa82384cd96ec95bc7a39a6dce37a6f2c78d217
João Pedro Oliveira <olliveiravb@gmail.com>
2024-12-28 17:55:41 -0300

    Up

--- init.lua
+++ init.lua
@@ -23,6 +23,9 @@ require("packer").startup(function(use)
   -- Adicionando o tema Catppuccin
   use { "catppuccin/nvim", as = "catppuccin" }
 
+  -- Adicionando o ToggleTerm
+  use "akinsho/toggleterm.nvim"
+
   if packer_bootstrap then
     require("packer").sync()
   end
@@ -38,6 +41,34 @@ require("nvim-tree").setup({
 
 -- Atalho para abrir a sidebar
 vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+-- Configuração dos Keymaps Personalizados
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Abrir o terminal com <Ctrl-t>
+keymap("n", "<C-t>", ":ToggleTerm<CR>", opts)
+
+-- Abrir o nvim-tree com <Ctrl-n>
+keymap("n", "<C-n>", ":NvimTreeToggle<CR>", opts)
+
+-- Criar um novo arquivo com <leader>n
+keymap("n", "<leader>n", ":e <C-R>=expand('%:p:h') . '/' <CR>", opts)
+
+-- Mostrar o diretório atual com <leader>d
+keymap("n", "<leader>d", ":echo expand('%:p:h')<CR>", opts)
+
+-- Criar uma nova pasta com <leader>p
+keymap("n", "<leader>p", function()
+  local folder_name = vim.fn.input("Nome da Pasta: ", "", "file")
+  local current_dir = vim.fn.expand("%:p:h")
+  local full_path = current_dir .. "/" .. folder_name
+  if folder_name ~= "" then
+    vim.fn.mkdir(full_path, "p")
+    print("Pasta criada: " .. full_path)
+  else
+    print("Nenhuma pasta criada.")
+  end
+end, opts)
 
 -- Ativar o mouse
 vim.o.mouse = "a"
@@ -57,3 +88,23 @@ require("catppuccin").setup({
 
 -- Ative o tema
 vim.cmd([[colorscheme catppuccin]])
+
+-- Configuração do ToggleTerm
+require("toggleterm").setup({
+  size = 20, -- Altura do terminal para terminais horizontais
+  open_mapping = [[<C-\>]], -- Atalho para abrir/fechar o terminal
+  direction = "horizontal", -- Direção do terminal: horizontal, vertical, tab, float
+  float_opts = {
+    border = "curved", -- Borda do terminal flutuante
+  },
+})
+
+-- Exemplo de terminal personalizado (opcional)
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _LAZYGIT_TOGGLE()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", { noremap = true, silent = true })
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -103,6 +103,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "C:\\Users\\joao\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  ["toggleterm.nvim"] = {
+    loaded = true,
+    path = "C:\\Users\\joao\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\toggleterm.nvim",
+    url = "https://github.com/akinsho/toggleterm.nvim"
   }
 }
 
