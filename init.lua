54b3dcf00ae3bcea31a1a3c3efef3b67a2480e78
João Pedro Oliveira <olliveiravb@gmail.com>
2025-04-11 11:37:03 -0300

    Fix

--- init.lua
+++ init.lua
@@ -1,16 +1,16 @@
--- Carrega configurações básicas
-require("options")
-require("keymaps")
-
--- Bootstrap do Lazy.nvim
+-- Bootstrap do Lazy.nvim (TEM QUE VIR PRIMEIRO)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
         "git", "clone", "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git", lazypath
+        "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath
     })
 end
 vim.opt.rtp:prepend(lazypath)
+
+-- Carrega configurações básicas
+require("options")
+require("keymaps")
 
 -- Carrega plugins a partir do arquivo plugins.lua
 require("lazy").setup("plugins")
@@ -18,4 +18,5 @@ require("lazy").setup("plugins")
 -- Configura LSP e autocompletion
 require("lsp")
 
+-- Atalhos com popup
 require("whichkey")
