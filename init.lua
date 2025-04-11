ad11e467040de7bbdac04c10c1a3811442469f0b
João Pedro Oliveira <olliveiravb@gmail.com>
2025-04-11 07:04:46 -0300

    Up

--- init.lua
+++ init.lua
@@ -2,8 +2,20 @@
 require("options")
 require("keymaps")
 
--- Instala e carrega plugins
-require("plugins")
+-- Bootstrap do Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git", lazypath
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Carrega plugins a partir do arquivo plugins.lua
+require("lazy").setup("plugins")
 
 -- Configura LSP e autocompletion
 require("lsp")
+
+require("whichkey")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,37 +1,157 @@
--- bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git", lazypath
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
+return {
   -- Aparência
-  "nvim-tree/nvim-tree.lua",
   "nvim-tree/nvim-web-devicons",
-  "nvim-lualine/lualine.nvim",
-  "catppuccin/nvim",
+  "catppuccin/nvim", -- ou troque por "folke/tokyonight.nvim" se preferir
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup({})
+    end,
+  },
 
-  -- Fuzzy finder
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
-
-  -- LSP e autocompletion
-  "neovim/nvim-lspconfig",
-  "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-nvim-lsp",
-  "L3MON4D3/LuaSnip",
-
-  "nvimtools/none-ls.nvim", -- null-ls (rebatizado)
-
+  -- NvimTree (explorer)
   {
     "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" }, -- ícones bonitinhos
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("nvim-tree").setup({})
       vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Alternar Explorer" })
     end,
-  }
-})
+  },
+
+  -- Telescope (fuzzy finder)
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({})
+      vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Buscar arquivos" })
+      vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Grep texto" })
+    end,
+  },
+
+  -- LSP e Autocompletion
+  "neovim/nvim-lspconfig",
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver", "jsonls", "html", "cssls" },
+        automatic_installation = true,
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+        },
+      })
+    end,
+  },
+
+  -- none-ls (formatação e linting)
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.diagnostics.eslint,
+        },
+      })
+    end,
+  },
+
+  -- Git
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end,
+  },
+  {
+    "sindrets/diffview.nvim",
+    dependencies = "nvim-lua/plenary.nvim"
+  },
+
+  -- Diagnóstico
+  {
+    "folke/trouble.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("trouble").setup({})
+      vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<CR>", { desc = "Toggle Trouble" })
+    end,
+  },
+  {
+    "simrat39/symbols-outline.nvim",
+    config = function()
+      require("symbols-outline").setup()
+      vim.keymap.set("n", "<leader>so", "<cmd>SymbolsOutline<CR>", { desc = "Outline" })
+    end,
+  },
+
+  -- Comentários
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end,
+  },
+
+  -- Atalhos com popup
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup()
+    end,
+  },
+
+  -- Terminal embutido
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup()
+      vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<CR>", { desc = "Abrir terminal" })
+    end,
+  },
+
+  -- Autopairs
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup({})
+    end,
+  },
+
+  -- Surround
+  {
+    "echasnovski/mini.surround",
+    version = "*",
+    config = function()
+      require("mini.surround").setup()
+    end,
+  },
+}
